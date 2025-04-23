36856e008e8beb35f837cd6af0059c5cbc96913f
yairbh <yairbh@gmail.com>
2025-04-23 19:30:23 +0300

    Updates

---
+++ _next/static/chunks/app/layout-8d23af045bb1f134.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1226:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,6146,23))},6146:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[979,497,954,358],()=>s(1226)),_N_E=e.O()}]);
